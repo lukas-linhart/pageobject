6dbbe77676e4da089c58ccdf57c5c2112b0054e3
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-16 10:56:11 +0200

    short name tests

---
+++ tests/test_pageobject.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+import sys
+here = os.path.abspath(os.path.dirname(__file__))
+parent_dir = '{}/..'.format(here)
+sys.path.append(parent_dir)
+
+import pageobject as po
+
+
+class PageObjectTests(unittest.TestCase):
+
+    def test_root_PO_has_correct_short_implicit_name(self):
+        root_po = po.PageObject('', None)
+        self.assertEqual(root_po.name, po.PageObject.DEFAULT_ROOT_NAME)
+
+
+    def test_root_PO_has_correct_short_explicit_name(self):
+        po_name = 'explicit_name'
+        root_po = po.PageObject('', None, name=po_name)
+        self.assertEqual(root_po.name, po_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
