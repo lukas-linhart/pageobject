4490a8a11f190c69dd9555d3450129d5d156a86e
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-16 12:12:01 +0200

    root PO full_name test

--- tests/test_pageobject.py
+++ tests/test_pageobject.py
@@ -26,6 +26,11 @@ class PageObjectTests(unittest.TestCase):
         self.assertEqual(root_po.name, po_name)
 
 
+    def test_root_PO_has_the_same_short_and_full_name(self):
+        root_po = po.PageObject('', None)
+        self.assertEqual(root_po.name, root_po.full_name)
+
+
     def test_nested_PO_has_correct_short_implicit_name(self):
         root_po = po.PageObject('', None)
         root_po.nested_po = po.PageObject('', root_po)
