a8d5fee1211dc2276ea3f764407cab84188a7c21
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-20 20:19:35 +0200

    POL index calls itself recursively when it fails due to stale element

--- pageobject/pageobjectlist.py
+++ pageobject/pageobjectlist.py
@@ -1,5 +1,6 @@
 from .pageobjectbase import PageObjectBase
 from .pageobject import PageObject
+from selenium.common.exceptions import StaleElementReferenceException
 
 
 class PageObjectList(PageObjectBase):
@@ -79,11 +80,16 @@ class PageObjectList(PageObjectBase):
         return len(self.children)
 
 
-    def index(self, value):
-        self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
-        self.logger.debug('getting index of "{}" within page object; {}'.format(value, self._log_id_long))
-        index = [item.text for item in self[:]].index(value)
-        self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
-        self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
-        return index
+    def index(self, value, attempt=1):
+        if attempt == 1:
+            self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
+        self.logger.debug('getting index of "{}" within page object, attempt number {}; {}'.format(value, attempt, self._log_id_long))
+        try:
+            index = [item.text for item in self[:]].index(value)
+            self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
+            self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
+            return index
+        except StaleElementReferenceException:
+            self.logger.debug('getting index failed, trying again')
+            return self.index(value, attempt=attempt+1)
 
