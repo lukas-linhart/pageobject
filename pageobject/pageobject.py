cdee534da34f8d8a4bff61f3553a971391e9d260
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-23 16:47:10 +0200

    added default error_msg to wait_until method when none is provided

--- pageobject/pageobject.py
+++ pageobject/pageobject.py
@@ -109,12 +109,17 @@ class PageObject(PageObjectBase):
         return visible
 
 
-    def wait_until(self, func, timeout=DEFAULT_WAIT_TIMEOUT, func_args=[], func_kwargs={}, error_msg='', reverse=False):
+    def wait_until(self, func, func_args=[], func_kwargs={},
+            timeout=DEFAULT_WAIT_TIMEOUT, error_msg=None, reverse=False):
         deadline = time.time() + timeout
         while time.time() < deadline:
             if func(*func_args, **func_kwargs) is not reverse:
                 return self
             time.sleep(PageObject.DEFAULT_POLL_INTERVAL)
+        if error_msg is None:
+            error_msg = ('function {} called with args {} and kwargs '
+                        + '{} still returns {} after {} seconds').format(
+                    func, func_args, func_kwargs, reverse, timeout)
         raise TimeoutException(error_msg)
 
 
