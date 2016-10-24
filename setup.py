2f8392f08580224f0a978125377cc0d296279037
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-24 07:57:31 +0200

    v0.0.18

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = 'Page Object design pattern implementation' + \
 setup_args = {
     'name': 'pageobject',
     'packages': ['pageobject'],
-    'version': '0.0.17',
+    'version': '0.0.18',
     'description': 'Page Object implementation',
     'long_description': long_description,
     'url': 'https://github.com/lukas-linhart/pageobject',
