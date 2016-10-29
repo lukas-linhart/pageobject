0d1f14aaf9af46a29df2d313be21deae2d6dc8ab
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-29 18:25:18 +0200

    PO dunder getitem unit tests

--- tests/unit/test_pageobject.py
+++ tests/unit/test_pageobject.py
@@ -7,9 +7,22 @@ def test_dunder_bool_method_returns_True_when_is_existing(monkeypatch):
     monkeypatch.setattr(po, 'is_existing', lambda log=False: True)
     assert bool(po)
 
-
 def test_dunder_bool_method_returns_False_when_is_not_existing(monkeypatch):
     po = PageObject('', None)
     monkeypatch.setattr(po, 'is_existing', lambda log=False: False)
     assert(bool(po) == False)
+
+
+
+def test_dunder_getitem_method_raises_KeyError_for_nonexisting_child(monkeypatch):
+    monkeypatch.setattr(PageObject, 'children', dict())
+    po = PageObject('', None)
+    with pytest.raises(KeyError):
+        po['nonexisting_child']
+
+def test_dunder_getitem_method_returns_correct_child():
+    root_po = PageObject('', None)
+    child_po = PageObject('', None)
+    root_po.child_po = PageObject('', root_po)
+    assert root_po['child_po'] == root_po.child_po
 
