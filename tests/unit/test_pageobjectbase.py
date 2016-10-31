3dd6c9f0b9fdb06c87cc176bf4f131533e8365b7
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-31 20:23:01 +0100

    refactored POB locator unit tests

--- tests/unit/test_pageobjectbase.py
+++ tests/unit/test_pageobjectbase.py
@@ -22,37 +22,26 @@ def test_parent_locator_returns_correct_value_when_parent_is_valid(monkeypatch, 
     mock_po_base.parent = Parent()
     assert mock_po_base._parent_locator == parent_locator
 
-
 def test_parent_locator_returns_empty_string_when_parent_is_invalid(mock_po_base):
     mock_po_base.parent = None
     assert mock_po_base._parent_locator == ''
-
 
 def test_locator_returns_default_locator_when_provided(monkeypatch, mock_po_base):
     locator = 'default'
     monkeypatch.setattr(mock_po_base.__class__, 'default_locator', locator)
     assert mock_po_base.locator == locator
 
+def test_locator_returns_correct_value_when_chained(monkeypatch, mock_po_base):
+    parent_locator = '//html'
+    child_locator = '//body'
+    mock_po_base._chain = True
+    monkeypatch.setattr(mock_po_base.__class__, '_parent_locator', parent_locator)
+    mock_po_base._locator = child_locator
+    assert mock_po_base.locator == '{}{}'.format(parent_locator, child_locator)
+
 def test_locator_returns_correct_value_when_not_chained(monkeypatch, mock_po_base):
     locator = '//body'
     mock_po_base._chain = False
     mock_po_base._locator = locator
-    assert mock_po_base.locator == locator
-
-def test_locator_returns_correct_value_when_chained_and_parent_exists(mock_po_base):
-    parent_locator = '//html'
-    child_locator = '//body'
-    class Parent:
-        locator = parent_locator
-    mock_po_base._chain = True
-    mock_po_base._locator = child_locator
-    mock_po_base.parent = Parent()
-    assert mock_po_base.locator == '{}{}'.format(parent_locator, child_locator)
-
-def test_locator_returns_correct_value_when_chained_but_not_a_child(monkeypatch, mock_po_base):
-    locator = '//body'
-    mock_po_base._chain = True
-    mock_po_base._locator = locator
-    mock_po_base.parent = None
     assert mock_po_base.locator == locator
 
