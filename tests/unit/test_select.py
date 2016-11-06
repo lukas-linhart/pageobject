f5e5519c0b5de65f0df68ad442c127c951a30291
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-06 19:45:01 +0100

    added Select._select_class unit test

--- tests/unit/test_select.py
+++ tests/unit/test_select.py
@@ -3,6 +3,10 @@ from .fixtures import mock_select
 from selenium.webdriver.support.ui import Select as WebDriverSelect
 
 
+def test_select_class_property_returns_a_webdriver_select_subclass(mock_select):
+    assert issubclass(mock_select._select_class, WebDriverSelect)
+
+
 def test_elem_property_returns_an_instance_of_webdriver_select(monkeypatch, mock_select):
     class MockWebDriverSelect(WebDriverSelect):
         def __init__(self, wd): pass
