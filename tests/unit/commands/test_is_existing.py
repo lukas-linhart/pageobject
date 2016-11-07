29709d06f6aad25d55bfbdb4acd1d742941cfb5b
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-07 07:40:02 +0100

    added commands.is_existing unit tests

---
+++ tests/unit/commands/test_is_existing.py
@@ -0,0 +1,15 @@
+import pytest
+from tests.unit.fixtures import mock_commands_po as mock_po
+from selenium.common.exceptions import WebDriverException
+
+
+def test_is_existing_method_returns_True_if_no_exception(mock_po):
+    assert mock_po.is_existing() == True
+
+def test_is_existing_method_returns_False_if_exception(monkeypatch, mock_po):
+    @property
+    def failing_webelement(self):
+        raise WebDriverException
+    monkeypatch.setattr(mock_po.__class__, 'webelement', failing_webelement)
+    assert mock_po.is_existing() == False
+
