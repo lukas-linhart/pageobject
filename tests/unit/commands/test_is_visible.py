6eeee4fefbb7d5c270d403d5a1583b03bfab7953
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-08 07:32:01 +0100

    added commands.is_visible unit test

---
+++ tests/unit/commands/test_is_visible.py
@@ -0,0 +1,8 @@
+import pytest
+from tests.unit.fixtures import mock_commands_po as mock_po
+
+
+def test_is_visible_method_returns_bool(mock_po):
+    mock_po.webelement.is_displayed = lambda log=True: True
+    assert isinstance(mock_po.is_visible(), bool)
+
