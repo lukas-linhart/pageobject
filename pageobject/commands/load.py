24c9711ad08697d8199ba4aff1bad98cdf4c8c77
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-13 19:21:01 +0100

    added logging to Page.load method

--- pageobject/commands/load.py
+++ pageobject/commands/load.py
@@ -1,2 +1,7 @@
-def load(self):
+def load(self, log=True):
+    if log:
+        self.logger.info('loading page (url "{}")'.format(self._url))
     self.webdriver.get(self._url)
+    if log:
+        self.logger.info('page loaded (url "{}")'.format(self._url))
+
