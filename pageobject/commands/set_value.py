8daf054b75dc04a648f8a8e3a9ed584030b219b0
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-14 07:48:08 +0100

    added press_enter to set_value command

--- pageobject/commands/set_value.py
+++ pageobject/commands/set_value.py
@@ -1,7 +1,12 @@
-def set_value(self, value):
+from selenium.webdriver.common.keys import Keys
+
+
+def set_value(self, value, press_enter=False):
     self.clear()
     self.logger.info('setting value of {} to "{}"'.format(self._log_id_short, value))
     self.logger.debug('setting value of page object to "{}"; {}'.format(value, self._log_id_long))
     self.webelement.send_keys(value)
+    if press_enter:
+        self.webelement.send_keys(Keys.ENTER)
     return self
 
