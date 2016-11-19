ab923414352350a14fe47724ef3d6d969b02b47c
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-11-19 19:27:26 +0100

    is_existing command docstr

--- pageobject/commands/is_existing.py
+++ pageobject/commands/is_existing.py
@@ -2,6 +2,13 @@ from selenium.common.exceptions import WebDriverException
 
 
 def is_existing(self, log=True):
+    """
+    Return True if page object exists in the DOM, False otherwise.
+
+    :param bool log: whether to log or not (default is True)
+    :returns: whether page object exists in the DOM
+    :rtype: :py:obj:`bool`
+    """
     if log:
         self.logger.info('determining whether {} is existing'.format(self._log_id_short))
         self.logger.debug('determining whether page object is existing; {}'.format(self._log_id_long))
