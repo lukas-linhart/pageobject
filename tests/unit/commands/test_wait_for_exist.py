24ea2cf3324b4b5903e5db1a808db029fc8feb6e
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-08 20:00:44 +0100

    wait_for_exist unit tests

---
+++ tests/unit/commands/test_wait_for_exist.py
@@ -0,0 +1,19 @@
+import pytest
+from tests.unit.fixtures import mock_commands_po as mock_po
+
+
+def test_wait_for_exist_returns_self_when_all_is_fine(monkeypatch, mock_po):
+    monkeypatch.setattr(mock_po.__class__, '_locator_value', 'locator')
+    mock_po.wait_until = lambda func, func_kwargs={}, timeout=None, error_msg='': True
+    assert mock_po.wait_for_exist() == mock_po
+
+def test_wait_for_exist_passes_correct_function_to_wait_until(monkeypatch, mock_po):
+    monkeypatch.setattr(mock_po.__class__, '_locator_value', 'locator')
+    mock_po.is_existing_called = False
+    def is_existing(log=False):
+        mock_po.is_existing_called = True
+        return True
+    mock_po.is_existing = is_existing
+    mock_po.wait_for_exist()
+    assert mock_po.is_existing_called == True
+
