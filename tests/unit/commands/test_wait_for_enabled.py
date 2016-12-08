09e0a891f25aae5d3fd3889c0fe6eda51d382fd9
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-08 20:05:40 +0100

    wait_for_enabled unit tests

---
+++ tests/unit/commands/test_wait_for_enabled.py
@@ -0,0 +1,19 @@
+import pytest
+from tests.unit.fixtures import mock_commands_po as mock_po
+
+
+def test_wait_for_enabled_returns_self_when_all_is_fine(monkeypatch, mock_po):
+    monkeypatch.setattr(mock_po.__class__, '_locator_value', 'locator')
+    mock_po.wait_until = lambda func, func_kwargs={}, timeout=None, error_msg='': True
+    assert mock_po.wait_for_enabled() == mock_po
+
+def test_wait_for_enabled_passes_correct_function_to_wait_until(monkeypatch, mock_po):
+    monkeypatch.setattr(mock_po.__class__, '_locator_value', 'locator')
+    mock_po.is_enabled_called = False
+    def is_enabled(log=False):
+        mock_po.is_enabled_called = True
+        return True
+    mock_po.is_enabled = is_enabled
+    mock_po.wait_for_enabled()
+    assert mock_po.is_enabled_called == True
+
