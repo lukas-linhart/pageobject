478e4a543368168ef43dc6fb2c2eb85a43bd8861
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-09 19:47:13 +0100

    dropped recursion in POL.index command

--- pageobject/commands/index.py
+++ pageobject/commands/index.py
@@ -1,7 +1,4 @@
-from selenium.common.exceptions import StaleElementReferenceException
-
-
-def index(self, value, _attempt=1):
+def index(self, value):
     """
     Return index of the first child containing the specified value.
 
@@ -10,15 +7,10 @@ def index(self, value, _attempt=1):
     :rtype: int
     :raises ValueError: if the value is not found
     """
-    if _attempt == 1:
-        self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
-    self.logger.debug('getting index of "{}" within page object, attempt number {}; {}'.format(value, _attempt, self._log_id_long))
-    try:
-        index = [item.text for item in self[:]].index(value)
-        self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
-        self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
-        return index
-    except StaleElementReferenceException:
-        self.logger.debug('getting index failed, trying again')
-        return self.index(value, _attempt=_attempt+1)
+    self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
+    self.logger.debug('getting index of "{}" within page object; {}'.format(value, self._log_id_long))
+    index = [item.text for item in self[:]].index(value)
+    self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
+    self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
+    return index
 
