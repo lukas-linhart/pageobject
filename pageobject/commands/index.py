b250e0f85ba973e2ffc13d2c2c932a70b897b9a7
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-10 07:25:25 +0100

    POL.index method now indexes text_values

--- pageobject/commands/index.py
+++ pageobject/commands/index.py
@@ -9,7 +9,7 @@ def index(self, value):
     """
     self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
     self.logger.debug('getting index of "{}" within page object; {}'.format(value, self._log_id_long))
-    index = [item.text for item in self[:]].index(value)
+    index = self.text_values.index(value)
     self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
     self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
     return index
