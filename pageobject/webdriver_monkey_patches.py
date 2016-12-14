5d3a308b285e79bab40c1d645f06a8e5d75fbbc4
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-14 20:02:11 +0100

    simplified condition in __setattr__ webdriver monkey patch

--- pageobject/webdriver_monkey_patches.py
+++ pageobject/webdriver_monkey_patches.py
@@ -7,7 +7,7 @@ def __setattr__(self, attr_name, attr_value):
     Register self as parent if attribute value is a page object.
     """
     object.__setattr__(self, attr_name, attr_value)
-    if attr_name is not '_parent' and isinstance(attr_value, PageObjectBase):
+    if isinstance(attr_value, PageObjectBase):
         child = attr_value
         child.__dict__['_parent'] = self
 
