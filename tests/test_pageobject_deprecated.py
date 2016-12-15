1a556beaa91802b33c8b53f92ca8c84cdd2d92ff
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-15 08:43:06 +0100

    removed redundant tests from test_pageobject_deprecated

--- tests/test_pageobject_deprecated.py
+++ tests/test_pageobject_deprecated.py
@@ -16,38 +16,6 @@ class PageObjectTests(unittest.TestCase):
         root_po.another_nested_po = PageObject('', root_po)
         self.assertEqual(len(root_po), len(root_po.children))
 
-    def test_root_PO_has_correct_short_implicit_name(self):
-        root_po = PageObject('', None)
-        self.assertEqual(root_po.name, root_po.__class__.DEFAULT_ROOT_NAME)
-
-
-    def test_root_PO_has_correct_short_explicit_name(self):
-        po_name = 'explicit_name'
-        root_po = PageObject('', None, name=po_name)
-        self.assertEqual(root_po.name, po_name)
-
-
-    def test_root_PO_has_the_same_short_and_full_name(self):
-        root_po = PageObject('', None)
-        self.assertEqual(root_po.name, root_po.full_name)
-
-
-    def test_nested_PO_has_correct_short_implicit_name(self):
-        root_po = PageObject('')
-        root_po.nested_po = PageObject('')
-        self.assertEqual(root_po.nested_po.name, 'nested_po')
-
-
-    def test_nested_PO_has_correct_full_name(self):
-        root_po, nested_po, leaf_po = (PageObject('') for x in range(3))
-        root_po.nested_po = nested_po
-        nested_po.leaf_po = leaf_po
-        separator = PageObject.NAME_SEPARATOR
-        leaf_po_full_name = '{}{}{}{}{}'.format(
-                root_po.name, separator, nested_po.name,
-                separator, leaf_po.name)
-        self.assertEqual(leaf_po.full_name, leaf_po_full_name)
-
 
     def test_root_PO_has_correct_chained_locator(self):
         locator = '//body'
