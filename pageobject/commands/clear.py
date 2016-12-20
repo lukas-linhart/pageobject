8dc244c7c24907f68b613b8ebfb97c42925bf04a
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-20 06:42:55 +0100

    clear command logging - reflected _log_id_short changes

--- pageobject/commands/clear.py
+++ pageobject/commands/clear.py
@@ -15,16 +15,16 @@ def clear(self, log=True, press_enter=False):
         or doesn't select an element
     """
     if log:
-        self.logger.info('clearing {}'.format(self._log_id_short))
+        self.logger.info('clearing page object {}'.format(self._log_id_short))
         self.logger.debug('clearing page object; {}'.format(self._log_id_long))
     self.webelement.clear()
     if log:
-        self.logger.info('{} cleared'.format(self._log_id_short))
+        self.logger.info('page object {} cleared'.format(self._log_id_short))
         self.logger.debug('page object cleared; {}'.format(self._log_id_long))
     if press_enter:
         self.webelement.send_keys(Keys.ENTER)
         if log:
-            self.logger.info('"enter" key sent to {}'.format(self._log_id_short))
+            self.logger.info('"enter" key sent to page object {}'.format(self._log_id_short))
             self.logger.debug('"enter" key sent to page object; {}'.format(self._log_id_long))
     return self
 
