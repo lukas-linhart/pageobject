8e0680013bd2b4791129aac386d48be54bef3a5b
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-20 06:44:42 +0100

    clear command - debug logging even if log parameter is false

--- pageobject/commands/clear.py
+++ pageobject/commands/clear.py
@@ -16,15 +16,15 @@ def clear(self, log=True, press_enter=False):
     """
     if log:
         self.logger.info('clearing page object {}'.format(self._log_id_short))
-        self.logger.debug('clearing page object; {}'.format(self._log_id_long))
+    self.logger.debug('clearing page object; {}'.format(self._log_id_long))
     self.webelement.clear()
     if log:
         self.logger.info('page object {} cleared'.format(self._log_id_short))
-        self.logger.debug('page object cleared; {}'.format(self._log_id_long))
+    self.logger.debug('page object cleared; {}'.format(self._log_id_long))
     if press_enter:
         self.webelement.send_keys(Keys.ENTER)
         if log:
             self.logger.info('"enter" key sent to page object {}'.format(self._log_id_short))
-            self.logger.debug('"enter" key sent to page object; {}'.format(self._log_id_long))
+        self.logger.debug('"enter" key sent to page object; {}'.format(self._log_id_long))
     return self
 
