9cbdc64bcc1144b8ca7d32d08aa5d36afa7f1e73
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-21 04:46:54 +0100

    index command - reflected _log_id_short change

--- pageobject/commands/index.py
+++ pageobject/commands/index.py
@@ -7,10 +7,10 @@ def index(self, value):
     :rtype: int
     :raises ValueError: if the value is not found
     """
-    self.logger.info('getting index of "{}" within {}'.format(value, self._log_id_short))
-    self.logger.debug('getting index of "{}" within page object; {}'.format(value, self._log_id_long))
+    self.logger.info('getting index of text "{}" within page object list {}'.format(value, self._log_id_short))
+    self.logger.debug('getting index of text "{}" within page object list; {}'.format(value, self._log_id_long))
     index = self.text_values.index(value)
-    self.logger.info('index of "{}" within {} is {}'.format(value, self._log_id_short, index))
-    self.logger.debug('index of "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
+    self.logger.info('index of text "{}" within page object list {} is {}'.format(value, self._log_id_short, index))
+    self.logger.debug('index of text "{}" within page object is {}; {}'.format(value, index, self._log_id_long))
     return index
 
