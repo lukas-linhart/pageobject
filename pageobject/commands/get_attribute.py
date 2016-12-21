abd181d6a135131d3e31ff3569d188174e351ee1
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-21 04:39:23 +0100

    get_attribute - reflected _log_id_short change

--- pageobject/commands/get_attribute.py
+++ pageobject/commands/get_attribute.py
@@ -11,7 +11,7 @@ def get_attribute(self, attribute, log=True):
         or doesn't select an element
     """
     if log:
-        self.logger.info('getting attribute "{}" of {}'.format(attribute, self._log_id_short))
+        self.logger.info('getting attribute "{}" of page object {}'.format(attribute, self._log_id_short))
         self.logger.debug('getting attribute "{}" of page object; {}'.format(attribute, self._log_id_long))
     return self.webelement.get_attribute(attribute)
 
