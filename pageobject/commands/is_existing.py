94dc996f4120947dcd4055f6d5ceb3b3ff62b365
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-21 04:51:16 +0100

    is_existing command - reflected _log_id_short change

--- pageobject/commands/is_existing.py
+++ pageobject/commands/is_existing.py
@@ -7,20 +7,20 @@ def is_existing(self, log=True):
 
     :param bool log: whether to log or not (default is True)
     :returns: whether page object exists in the DOM
-    :rtype: :py:obj:`bool`
+    :rtype: bool
     """
     if log:
-        self.logger.info('determining whether {} is existing'.format(self._log_id_short))
+        self.logger.info('determining whether page object {} is existing'.format(self._log_id_short))
         self.logger.debug('determining whether page object is existing; {}'.format(self._log_id_long))
     try:
         self.webelement
     except WebDriverException:
         if log:
-            self.logger.info('{} is not existing'.format(self._log_id_short))
+            self.logger.info('page object {} is not existing'.format(self._log_id_short))
             self.logger.debug('page object is not existing; {}'.format(self._log_id_long))
         return False
     if log:
-        self.logger.info('{} is existing'.format(self._log_id_short))
+        self.logger.info('page object {} is existing'.format(self._log_id_short))
         self.logger.debug('page object is existing; {}'.format(self._log_id_long))
     return True
 
