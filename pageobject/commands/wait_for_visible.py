348c396d7105feb245ade36403eb2b88c4f0ce71
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-22 06:08:26 +0100

    wait_for_visible command logging

--- pageobject/commands/wait_for_visible.py
+++ pageobject/commands/wait_for_visible.py
@@ -3,17 +3,17 @@ def wait_for_visible(self, timeout=None):
     Wait for page object to be visible.
 
     :param int timeout: number of seconds to wait, if not provided
-        :py:obj:`PageObject.DEFAULT_WAIT_TIMEOUT` is used
+        PageObject.DEFAULT_WAIT_TIMEOUT is used
     """
     if timeout is None:
         timeout = self.DEFAULT_WAIT_TIMEOUT
-    self.logger.info('waiting until {} is visible'.format(self._log_id_short))
+    self.logger.info('waiting until page object {} is visible'.format(self._log_id_short))
     self.logger.debug('waiting until page object is visible; {}'.format(self._log_id_long))
-    error_msg = 'Element "{}" still not visible after {} seconds'.format(
-        self._locator_value, timeout)
+    error_msg = 'page object still not visible after {} seconds; {}'.format(
+        timeout, self._log_id_long)
     self.wait_until(self.is_visible, func_kwargs=dict(log=False),
         timeout=timeout, error_msg=error_msg)
-    self.logger.info('finished waiting until {} is visible'.format(self._log_id_short))
+    self.logger.info('finished waiting until page object {} is visible'.format(self._log_id_short))
     self.logger.debug('finished waiting until page object is visible; {}'.format(self._log_id_long))
     return self
 
