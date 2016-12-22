287bfe4b9dc8489b651c43d061ab92de8eb85fe0
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-22 05:49:29 +0100

    wait_for_enabled command logging

--- pageobject/commands/wait_for_enabled.py
+++ pageobject/commands/wait_for_enabled.py
@@ -3,17 +3,17 @@ def wait_for_enabled(self, timeout=None):
     Wait for page object to be enabled.
 
     :param int timeout: number of seconds to wait, if not provided
-        :py:obj:`PageObject.DEFAULT_WAIT_TIMEOUT` is used
+        PageObject.DEFAULT_WAIT_TIMEOUT is used
     """
     if timeout is None:
         timeout = self.DEFAULT_WAIT_TIMEOUT
-    self.logger.info('waiting until {} is enabled'.format(self._log_id_short))
+    self.logger.info('waiting until page object {} is enabled'.format(self._log_id_short))
     self.logger.debug('waiting until page object is enabled; {}'.format(self._log_id_long))
-    error_msg = 'Element "{}" still not enabled after {} seconds'.format(
-        self._locator_value, timeout)
+    error_msg = 'page object still not enabled after {} seconds; {}'.format(
+        timeout, self._log_id_long)
     self.wait_until(self.is_enabled, func_kwargs=dict(log=False),
         timeout=timeout, error_msg=error_msg)
-    self.logger.info('finished waiting until {} is enabled'.format(self._log_id_short))
+    self.logger.info('finished waiting until page object {} is enabled'.format(self._log_id_short))
     self.logger.debug('finished waiting until page object is enabled; {}'.format(self._log_id_long))
     return self
 
