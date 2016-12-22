9e13eabb4915e25727e95805da56a3d24c8e312a
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-22 06:18:01 +0100

    improved wait_for_xyz commands formatting

--- pageobject/commands/wait_for_enabled.py
+++ pageobject/commands/wait_for_enabled.py
@@ -7,13 +7,22 @@ def wait_for_enabled(self, timeout=None):
     """
     if timeout is None:
         timeout = self.DEFAULT_WAIT_TIMEOUT
-    self.logger.info('waiting until page object {} is enabled'.format(self._log_id_short))
-    self.logger.debug('waiting until page object is enabled; {}'.format(self._log_id_long))
-    error_msg = 'page object still not enabled after {} seconds; {}'.format(
-        timeout, self._log_id_long)
+
+    self.logger.info(('waiting until page object {} is enabled'
+        ).format(self._log_id_short))
+    self.logger.debug(('waiting until page object is enabled; {}'
+        ).format(self._log_id_long))
+
+    error_msg = ('page object still not enabled after {} seconds; {}'
+        ).format(timeout, self._log_id_long)
+
     self.wait_until(self.is_enabled, func_kwargs=dict(log=False),
         timeout=timeout, error_msg=error_msg)
-    self.logger.info('finished waiting until page object {} is enabled'.format(self._log_id_short))
-    self.logger.debug('finished waiting until page object is enabled; {}'.format(self._log_id_long))
+
+    self.logger.info(('finished waiting until page object {} is enabled'
+        ).format(self._log_id_short))
+    self.logger.debug(('finished waiting until page object is enabled; {}'
+        ).format(self._log_id_long))
+
     return self
 
