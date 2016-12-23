412694cbb2bc277c599da8f5db33be9ff976fcb4
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-23 06:53:00 +0100

    test is_displayed instead of is_visible

--- tests/unit/commands/test_is_displayed.py
+++ tests/unit/commands/test_is_displayed.py
@@ -1,7 +1,7 @@
 from tests.unit.fixtures import mock_commands_po as mock_po
 
 
-def test_is_visible_method_delegates_to_webelement_and_returns_bool(mock_po):
+def test_is_displayed_method_delegates_to_webelement_and_returns_bool(mock_po):
     mock_po.webelement.is_displayed = lambda log=True: True
-    assert isinstance(mock_po.is_visible(), bool)
+    assert isinstance(mock_po.is_displayed(), bool)
 
