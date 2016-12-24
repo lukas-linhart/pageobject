3b25d065937b2c818ff5e8c5d37495880efc8fe8
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-24 08:52:30 +0100

    adjusted wait_until_displayed logging and docstr

--- pageobject/commands/wait_until_displayed.py
+++ pageobject/commands/wait_until_displayed.py
@@ -1,6 +1,6 @@
 def wait_until_displayed(self, timeout=None):
     """
-    Wait for page object to be visible.
+    Wait until page object to be displayed.
 
     :param int timeout: number of seconds to wait, if not provided
         PageObject.DEFAULT_WAIT_TIMEOUT is used
@@ -8,20 +8,20 @@ def wait_until_displayed(self, timeout=None):
     if timeout is None:
         timeout = self.DEFAULT_WAIT_TIMEOUT
 
-    self.logger.info(('waiting until page object {} is visible'
+    self.logger.info(('waiting until page object {} is displayed'
         ).format(self._log_id_short))
-    self.logger.debug(('waiting until page object is visible; {}'
+    self.logger.debug(('waiting until page object is displayed; {}'
         ).format(self._log_id_long))
 
-    error_msg = ('page object still not visible after {} seconds; {}'
+    error_msg = ('page object still not displayed after {} seconds; {}'
         ).format(timeout, self._log_id_long)
 
     self.wait_until(self.is_displayed, func_kwargs=dict(log=False),
         timeout=timeout, error_msg=error_msg)
 
-    self.logger.info(('finished waiting until page object {} is visible'
+    self.logger.info(('finished waiting until page object {} is displayed'
         ).format(self._log_id_short))
-    self.logger.debug(('finished waiting until page object is visible; {}'
+    self.logger.debug(('finished waiting until page object is displayed; {}'
         ).format(self._log_id_long))
 
     return self
