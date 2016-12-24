4f7ebdc36270ce9fb4f4150ca7f494979308a1a5
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-24 07:16:22 +0100

    is_displayed logging and docstr tweaks

--- pageobject/commands/is_displayed.py
+++ pageobject/commands/is_displayed.py
@@ -1,23 +1,30 @@
 def is_displayed(self, log=True):
     """
-    Return True if page object is visible, False otherwise.
+    Return True if page object is displayed, False otherwise.
+
+    :param bool log: whether to log or not (default is True)
+    :returns: whether page object is displayed
+    :rtype: bool
+    """
+    if log:
+        self.logger.info('determining whether page object {} is displayed'.format(self._log_id_short))
+    self.logger.debug('determining whether page object is displayed; {}'.format(self._log_id_long))
+    displayed = self.webelement.is_displayed()
+    neg_str = '' if displayed else ' not'
+    if log:
+        self.logger.info('page object {} is{} displayed'.format(self._log_id_short, neg_str))
+    self.logger.debug('page object is{} displayed; {}'.format(neg_str, self._log_id_long))
+    return displayed
+
+
+def is_visible(self, log=True):
+    """
+    DEPRECATED! Return True if page object is visible, False otherwise.
 
     :param bool log: whether to log or not (default is True)
     :returns: whether page object is visible
     :rtype: bool
     """
-    if log:
-        self.logger.info('determining whether page object {} is visible'.format(self._log_id_short))
-    self.logger.debug('determining whether page object is visible; {}'.format(self._log_id_long))
-    visible = self.webelement.is_displayed()
-    neg_str = '' if visible else ' not'
-    if log:
-        self.logger.info('page object {} is{} visible'.format(self._log_id_short, neg_str))
-    self.logger.debug('page object is{} visible; {}'.format(neg_str, self._log_id_long))
-    return visible
-
-
-def is_visible(self, log=True):
     self.logger.warning('"is_visible" command is deprecated, use "is_displayed" instead!')
     return self.is_displayed(log=log)
 
