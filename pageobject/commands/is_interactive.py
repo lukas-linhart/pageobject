a0f6f90cdd24575a733e37e9617e08fe65386f9f
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-27 20:58:27 +0100

    is_interactive now uses is_displayed instead of is_visible

--- pageobject/commands/is_interactive.py
+++ pageobject/commands/is_interactive.py
@@ -2,11 +2,11 @@ def is_interactive(self, log=True):
     """
     Return True if page object is interactive, False otherwise.
 
-    Interactive means both visible and enabled. This is called
+    Interactive means both displayed and enabled. This is called
     "clickable" in selenium, which may be misleading,
-    as an element can be both visible and enabled, but not really
+    as an element can be both displayed and enabled, but not really
     clickable, because another element may cover it and prevent
-    it from receiving the click
+    it from receiving the click.
 
     :param bool log: whether to log or not (defualt is True)
     :returns: whether page object is interactive
@@ -18,7 +18,7 @@ def is_interactive(self, log=True):
     self.logger.debug(('determining whether page object is interactive'
         + '; {}').format(self._log_id_long))
 
-    interactive = self.is_visible(log=False) and self.is_enabled(log=False)
+    interactive = self.is_displayed(log=False) and self.is_enabled(log=False)
 
     msg_str = '' if interactive else ' not'
     if log:
--- tests/unit/commands/test_is_interactive.py
+++ tests/unit/commands/test_is_interactive.py
@@ -1,19 +1,19 @@
 from tests.unit.fixtures import mock_commands_po as mock_po
 
-def test_is_interactive_returns_False_if_only_visible(mock_po):
-    mock_po.is_visible = lambda log=True: True
+def test_is_interactive_returns_False_if_only_displayed(mock_po):
+    mock_po.is_displayed = lambda log=True: True
     mock_po.is_enabled = lambda log=True: False
     assert mock_po.is_interactive() == False
 
 
 def test_is_interactive_returns_False_if_only_enabled(mock_po):
-    mock_po.is_visible = lambda log=True: False
+    mock_po.is_displayed = lambda log=True: False
     mock_po.is_enabled = lambda log=True: True
     assert mock_po.is_interactive() == False
 
 
-def test_is_interactive_returns_True_if_both_visible_and_enabled(mock_po):
-    mock_po.is_visible = lambda log=True: True
+def test_is_interactive_returns_True_if_both_displayed_and_enabled(mock_po):
+    mock_po.is_displayed = lambda log=True: True
     mock_po.is_enabled = lambda log=True: True
     assert mock_po.is_interactive() == True
 
