0867e7e0dba16f864ca80a80bcbfa6bd86881abe
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-28 08:53:43 +0100

    moved wait_for_vanish to standalone module

--- pageobject/commands/__init__.py
+++ pageobject/commands/__init__.py
@@ -15,7 +15,7 @@ from .wait_until_displayed import wait_until_displayed
 from .wait_until_vanished import wait_until_vanished
 from .wait_for_exist import wait_for_exist # deprecated
 from .wait_for_visible import wait_for_visible # deprecated
-from .wait_until_vanished import wait_for_vanish # deprecated
+from .wait_for_vanish import wait_for_vanish # deprecated
 from .wait_for_enabled import wait_for_enabled
 from .wait_for_interactive import wait_for_interactive
 from .click import click
