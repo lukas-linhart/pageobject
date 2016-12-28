52cdd6bed87b1388504663b4848f76c7e590af2e
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-28 08:44:54 +0100

    deprecated wait_for_vanish command

--- pageobject/commands/wait_until_vanished.py
+++ pageobject/commands/wait_until_vanished.py
@@ -27,5 +27,11 @@ def wait_until_vanished(self, timeout=None):
     return self
 
 
-wait_for_vanish = wait_until_vanished
+def wait_for_vanish(self, timeout=None):
+    """
+    DEPRECATED! Use wait_until_vanished command instead.
+    """
+    self.logger.warning('"wait_for_vanish" command is deprecated, use "wait_until_vanished" instead!')
+    self.wait_until_vanished(timeout=timeout)
+    return self
 
