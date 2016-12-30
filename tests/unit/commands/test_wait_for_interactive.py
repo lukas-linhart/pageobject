5ed11faeef2d1f6982c14ff2482fcc39e9ad615e
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-12-30 08:32:33 +0100

    added wait_for_interactive unit test

---
+++ tests/unit/commands/test_wait_for_interactive.py
@@ -0,0 +1,13 @@
+from tests.unit.fixtures import mock_commands_po as mock_po
+
+
+def test_wait_for_interactive_method_uses_wait_until_interactive_method(monkeypatch, mock_po):
+    mock_po.is_interactive_called = False
+    def wait_until_interactive(timeout=None):
+        mock_po.is_interactive_called = True
+        return mock_po
+    mock_po.wait_until_interactive = wait_until_interactive
+
+    assert mock_po.wait_for_interactive() == mock_po
+    assert mock_po.is_interactive_called == True
+
